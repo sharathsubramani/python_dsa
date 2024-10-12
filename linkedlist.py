355a7c720e22f8bfea41982b792b3a0f55543cd8
Sharath Subramani <sharathsubramani@Sharaths-MacBook-Air.local>
2024-10-12 22:46:37 +0530

    commit

--- linkedlist.py
+++ linkedlist.py
@@ -40,19 +40,31 @@ class LinkedList():
         prenode.next = None
         del temp
         # print(prenode.data,prenode.next,'prenode')
-        
+
     # def delete_node_from_pos(self):
-        
-        
+
+    def get_length_linkedlist(self):
+        temp = self.head
+        if self.head is None:
+            print("linked list is empty!")
+        count = 0
+
+        while temp:
+            count += 1
+            temp = temp.next
+        print('length of linked list is: ' , count)
+        return count
 
 
 if __name__ == '__main__':
     linkedlist = LinkedList()
     for i in range(1, 21):
         linkedlist.insert_node_at_begining(i)
+    # linkedlist.get_length_linkedlist()
     # linkedlist.insert_node_at_begining(4)
     # linkedlist.insert_node_at_begining(5)
     # linkedlist.s_print()
     # linkedlist.delete_node_from_begining()
-    linkedlist.delete_node_from_end()
+    # linkedlist.delete_node_from_end()
     linkedlist.s_print()
+    linkedlist.get_length_linkedlist()
